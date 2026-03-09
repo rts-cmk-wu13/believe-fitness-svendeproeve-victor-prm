a8ac70979b2d15e4e41ea48d07622cfe5b259b15
victor-prm <victorpabb@gmail.com>
2026-03-09 11:51:20 +0100

    style: add project fonts and colors

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <p className="text-8xl font-poppins">Poppins</p>
+      <p className="text-8xl">Inter</p>
+    </div>
+  );
+}
